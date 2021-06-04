37c77a72c155b21fde6955b623bc15137fd14684
oliverkarger <kmaster@oliver-karger.de>
2021-06-04 16:45:31 +0200

    Fix #1
    Signed-off-by: oliverkarger <kmaster@oliver-karger.de>

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
 import {
   faUserCircle,
   faHome,
   faBlog,
   faIdCard,
   faCog,
+  faToggleOff,
+  faToggleOn,
 } from '@fortawesome/free-solid-svg-icons';
 import {faGitAlt} from '@fortawesome/free-brands-svg-icons';
 
@@ -21,6 +23,11 @@ import {faGitAlt} from '@fortawesome/free-brands-svg-icons';
  * @class AppComponent
  */
 export class AppComponent {
+  @ViewChild('toggleNavButton', {read: ElementRef, static: false})
+  toggleNavButton?: ElementRef;
+  @ViewChild('navbar', {read: ElementRef, static: false})
+  navigation?: ElementRef;
+
   title = 'app';
   navUserCircleSymbol = faUserCircle;
   navProjectSymbol = faGitAlt;
@@ -28,4 +35,28 @@ export class AppComponent {
   navBlogIcon = faBlog;
   navContactIcon = faIdCard;
   loadingSymbol = faCog;
+  toggleNavIcon = faToggleOff;
+  navState: navMode = 'navbar-left';
+
+  /**
+   * @description Toggle navbar between sidenav and nav
+   * @author Oliver Karger <kmaster@oliver-karger.de>
+   * @date 04/06/2021
+   * @memberof AppComponent
+   */
+  toggleNav() {
+    if (this.navState === 'navbar-top') {
+      this.navigation?.nativeElement.classList.remove('navbar-top');
+      this.navigation?.nativeElement.classList.add('navbar-left');
+      this.navState = 'navbar-left';
+      this.toggleNavIcon = faToggleOff;
+    } else if (this.navState === 'navbar-left') {
+      this.navigation?.nativeElement.classList.remove('navbar-left');
+      this.navigation?.nativeElement.classList.add('navbar-top');
+      this.navState = 'navbar-top';
+      this.toggleNavIcon = faToggleOn;
+    }
+  }
 }
+
+type navMode = 'navbar-top' | 'navbar-left';
