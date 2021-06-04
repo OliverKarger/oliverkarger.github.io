2542d1d823749cdf03507c613bab4c5eff1b305a
Oliver Karger <kmaster@oliver-karger.de>
2021-06-04 08:29:28 +0000

    Initial Commit. - Created Angular basic Code - Included https://github.com/oliverkarger/css-colors

---
+++ app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+/**
+ * @description App ROuting Module
+ * @author Oliver Karger <kmaster@oliver-karger.de>
+ * @date 04/06/2021
+ * @export
+ * @class AppRoutingModule
+ */
+export class AppRoutingModule {}
---
+++ app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+/**
+ * @description Main App Component Class
+ * @author Oliver Karger <kmaster@oliver-karger.de>
+ * @date 04/06/2021
+ * @export
+ * @class AppComponent
+ */
+export class AppComponent {
+  title = 'app';
+}
